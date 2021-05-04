98c1b0dce151c1feeb620d6b258eba5ece692452
yukiho <is0436er@ed.ritsumei.ac.jp>
2021-05-05 03:46:58 +0900

    [Add] 起動時に自動的にマイグレーションを動かすように設定

--- db/migration/000009_create_reservation.up.sql
+++ db/migration/000009_create_reservation.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   `item_id` INT NOT NULL,
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_by` INT NOT NULL,
-  `updated_at` DATETIME NOT NULL DEFAULT DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_by` INT NOT NULL,
   INDEX `fk_reservation_user1_idx` (`user_id` ASC) VISIBLE,
   INDEX `fk_reservation_item1_idx` (`item_id` ASC) VISIBLE,
